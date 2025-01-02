ee0eba08bcf3026588ab95002e01dca5126d25af
madjin <32600939+madjin@users.noreply.github.com>
2025-01-01 22:07:11 -0500

    add elizaOS config

--- scripts/jsdoc-automation/src/Configuration.ts
+++ scripts/jsdoc-automation/src/Configuration.ts
@@ -40,7 +40,7 @@ export class Configuration implements Omit<ConfigurationData, 'rootDirectory'> {
 
     public excludedDirectories: string[] = [];
     public repository: Repository = {
-        owner: 'madjin',
+        owner: 'elizaOS',
         name: 'eliza',
         pullNumber: undefined
     };
