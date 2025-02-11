f45370fa8ce38d00ac4695607af748b9e38d69fb
Ting Chien Meng <tcm390@nyu.edu>
2025-02-12 02:46:36 +0800

    update dev script to use bash script

---
+++ scripts/dev.sh
@@ -0,0 +1,24 @@
+
+# An array of matching commands to run.
+COMMANDS=(
+  "turbo run dev --filter=./packages/core"
+  "turbo run dev --filter=./packages/plugin-sqlite"
+  "turbo run dev \
+     --filter=!./packages/agent \
+     --filter=!./packages/cli \
+     --filter=!./packages/docs \
+     --filter=!./packages/core \
+     --filter=!./packages/plugin-sqlite \
+     --concurrency=20"
+  "turbo run dev --filter=./packages/agent --filter=./packages/cli"
+)
+
+# Loop over each command/comment pair
+for i in "${!COMMANDS[@]}"; do
+  ${COMMANDS[$i]} &
+
+  sleep 3  # 3-second delay before starting each dev command
+done
+
+# Wait for all background jobs (dev servers) to keep the script alive
+wait
