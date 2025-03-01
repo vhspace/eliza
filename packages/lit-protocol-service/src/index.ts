15bb8e89104221f57b31a1397e3d39d767dda569
Mark Ballew (1898145) <ballew@berkeley.edu>
2025-03-01 03:13:40 -0800

    allow js to run with our lit package

--- packages/lit-protocol-service/src/index.ts
+++ packages/lit-protocol-service/src/index.ts
@@ -1,1 +1,11 @@
-export * from './lit-protocol'; 
+// Export JavaScript modules
+import * as litJs from './lit.js';
+import * as runDecryptJs from './run-decrypt.js';
+
+export { litJs, runDecryptJs };
+
+// Default export
+export default {
+  lit: litJs,
+  runDecrypt: runDecryptJs
+}; 
